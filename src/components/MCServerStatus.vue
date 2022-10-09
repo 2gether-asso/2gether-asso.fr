4f3f724abcdccd1b9780e814607156a5de907908
Matiboux <matiboux@gmail.com>
2022-10-09 05:38:42 +0200

    Add servers page and MCServerStatus component

---
+++ src/components/MCServerStatus.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+  props: ['name'],
+  data() {
+    return {
+      data: null
+    }
+  },
+  async created () {
+    try {
+      const response = await fetch(`https://api.2gether-asso.fr/mc/${this.name}`)
+      const data = await response.json()
+      this.data = data
+    }
+    catch (e) {
+      this.data = null
+    }
+  },
+}
+</script>
+
+<template>
+  <div>
+    <!-- Data fetched at build can be rendered in HTML -->
+    <h1>MC STATUS: {{ name }}</h1>
+
+    <div v-if="data === null">
+      <p>Chargement...</p>
+    </div>
+    <div v-else-if="data.status">
+      <img v-if="data.favicon" :src="data.favicon" />
+      <ul>
+        <li>Status : {{ data.status }}</li>
+        <li>IP : {{ data.address }}</li>
+        <li v-if="data.players">Players : {{ data.players.online }}/{{ data.players.max }}</li>
+        <li v-if="data.version">Version : {{ data.version.name }}</li>
+      </ul>
+    </div>
+    <div v-else>
+      <p v-if="data.error">Erreur : {{ data.error }}</p>
+      <p v-else>Erreur inconnue.</p>
+    </div>
+    <br>
+  </div>
+</template>
