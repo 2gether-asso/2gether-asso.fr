8c45fa98feda8a1bc7fedc45ef805859faaa9db9
Matiboux <matiboux@gmail.com>
2023-03-12 22:42:50 +0100

    Import modified astro-landing-page as layout base

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,36 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: [
 		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
 	],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ["InterVariable", ...defaultTheme.fontFamily.sans],
+			},
+			colors: {
+				primary: "var(--color-primary)",
+				secondary: "var(--color-secondary)",
+			},
+			textColor: {
+				default: "var(--color-text)",
+				offset: "var(--color-text-offset)",
+			},
+			backgroundColor: {
+				default: "var(--color-background)",
+				offset: "var(--color-background-offset)",
+			},
+			borderColor: {
+				default: "var(--color-border)",
+			},
+		},
 	},
-	plugins: [],
-}
+	corePlugins: {
+		fontSize: false,
+	},
+	plugins: [
+		require("tailwindcss-fluid-type"),
+	],
+};
