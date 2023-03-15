984da073138a0adfaa55520a65c0c929f3a897bb
Matiboux <matiboux@gmail.com>
2023-03-15 23:23:26 +0100

    Show motd in MC status

--- src/components/MCServerStatus.vue
+++ src/components/MCServerStatus.vue
@@ -5,6 +5,8 @@ export default {
   data() {
     return {
       data: null
+    } as {
+      data: any
     }
   },
   async created () {
@@ -39,28 +41,12 @@ export default {
       <p class="text-center">Chargement...</p>
 
     </div>
-    <div v-else-if="data.online">
+    <div v-else>
 
       <div v-if="data.icon" class="w-16 h-16 border-2 border-secondary rounded-2xl overflow-hidden mx-auto mb-2">
         <img :src="data.icon" />
       </div>
-
-      <h1 class="text-primary font-bold text-center">
-        {{ name }}
-      </h1>
-
-      <p>IP : {{ data.address }}</p>
-      <p>Status : {{ data.online ? '🟢 En ligne' : '🔴 Hors Ligne' }}</p>
-      <br />
-
-      <p v-if="data.players">Joueurs : {{ data.players.online }} / {{ data.players.max }}</p>
-      <p v-if="data.version">Version : {{ data.version }}</p>
-      <p v-if="data.seed">Seed : {{ data.seed }}</p>
-
-    </div>
-    <div v-else>
-
-      <div class="flex justify-center items-center bg-gray-200 w-16 h-16 border-2 border-secondary rounded-2xl overflow-hidden mx-auto mb-2">
+      <div v-else class="flex justify-center items-center bg-gray-200 w-16 h-16 border-2 border-secondary rounded-2xl overflow-hidden mx-auto mb-2">
         <svg class="w-6 fill-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
           <!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
           <path d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480H40c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24V296c0 13.3 10.7 24 24 24s24-10.7 24-24V184c0-13.3-10.7-24-24-24zm32 224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z" />
@@ -71,8 +57,22 @@ export default {
         {{ name }}
       </h1>
 
-      <p v-if="data.error">Erreur : {{ data.error }}</p>
-      <p v-else>Erreur inconnue.</p>
+      <div class="mb-2">
+        <p>IP : <code>{{ data.address || '[?]' }}</code></p>
+
+        <p v-if="data.online !== undefined">Status : {{ data.online ? '🟢 En ligne' : '🔴 Hors Ligne' }}</p>
+        <p v-else>Status : ❌ Erreur</p>
+      </div>
+
+      <p v-if="data.error" class="mb-2">Erreur : {{ data.error }}</p>
+
+      <div v-if="data.motd" v-html="data.motd.html[0]" class="mb-2"></div>
+
+      <div v-if="data.online">
+        <p v-if="data.players">Joueurs : {{ data.players.online }} / {{ data.players.max }}</p>
+        <p v-if="data.version">Version : {{ data.version }}</p>
+        <p v-if="data.seed">Seed : {{ data.seed }}</p>
+      </div>
 
     </div>
   </div>
